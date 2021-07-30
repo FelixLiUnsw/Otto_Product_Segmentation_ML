ae03f94d183d5d74946921e9dd6f6a07ffd96a46
sfc-97 <74003976+sfc-97@users.noreply.github.com>
2021-07-31 03:56:56 +0800

    Add files via upload

--- MLPClassifier.py
+++ MLPClassifier.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from datetime import datetime
-
+import warnings
+warnings.filterwarnings("ignore")
 
 # Multi-Class Log-Loss
 
@@ -34,17 +35,6 @@ data_y = data.iloc[:, 94:95]
 # test data import
 train_x, valid_x, train_y, valid_y = train_test_split(data_x, data_y, test_size=0.3, random_state=40)
 
-
-# TODO -----------
-# max_iter_list, pred_y_list = np.concatenate((np.arange(5, 51, 5), np.arange(60, 301, 20))), []
-max_iter_list, pred_y_list = np.arange(10, 30, 1), []
-
-for mi in max_iter_list:
-    mlp = MLPClassifier(max_iter=mi, shuffle=False)
-    mlp.fit(train_x, train_y.values.ravel())
-    pred_y_list.append(mlp.predict_proba(valid_x))
-# ----------------
-
 columns_list = ["Class_1", "Class_2", "Class_3", "Class_4", "Class_5", "Class_6", "Class_7", "Class_8", "Class_9"]
 
 yij = pd.DataFrame(0, index=np.arange(len(valid_y)), columns=columns_list)
@@ -52,16 +42,46 @@ for i in range(len(valid_y)):
     index = columns_list.index(valid_y.iloc[i].item())
     yij.iloc[i, index] = 1
 
+# find best parameters
 
-mcll_list = []
-for i in range(len(pred_y_list)):
-    Multi_class_log_loss = logloss(pred_y_list[i], yij)
-    mcll_list.append(Multi_class_log_loss)
-    print(f"The Multi Class Log Loss is {Multi_class_log_loss} when max_iter is {max_iter_list[i]}")
-
+# max_iter_list, activation_list, color = np.arange(15, 71, 5), ['relu', 'logistic', 'tanh'], ['red', 'green', 'blue']
+# for i in range(len(activation_list)):
+#     pred_y_list = []
+#     for it in max_iter_list:
+#         mlp = MLPClassifier(activation=activation_list[i], max_iter=it, shuffle=False, random_state=0)
+#         mlp.fit(train_x, train_y.values.ravel())
+#         pred_y_list.append(mlp.predict_proba(valid_x))
 #
+#     mcll_list = []
+#     for j in range(len(pred_y_list)):
+#         Multi_class_log_loss = logloss(pred_y_list[j], yij)
+#         mcll_list.append(Multi_class_log_loss)
+#         print(f"activition is {activation_list[i]} and max_iter is {max_iter_list[j]}: {Multi_class_log_loss}.")
+#
+#     plt.title('MLPClassifier')
+#     plt.plot(max_iter_list, mcll_list, color=color[i], label=activation_list[i])
+#
+# plt.xlabel('activation', fontsize=16)
+# plt.ylabel('Multi-Class Log Loss', fontsize=16)
+# plt.legend()
+# plt.show()
+
+# max_iter_list, pred_y_list = np.arange(40, 51, 1), []
+#
+# for mi in max_iter_list:
+#     mlp = MLPClassifier(solver='adam', max_iter=mi, shuffle=False, activation='logistic', random_state=0)
+#     mlp.fit(train_x, train_y.values.ravel())
+#     pred_y_list.append(mlp.predict_proba(valid_x))
+#
+# mcll_list = []
+# for i in range(len(pred_y_list)):
+#     Multi_class_log_loss = logloss(pred_y_list[i], yij)
+#     mcll_list.append(Multi_class_log_loss)
+#     print(f"The Multi Class Log Loss is {Multi_class_log_loss} when max_iter is {max_iter_list[i]}")
+#
+# # print plot
 # fig = plt.figure()
-# fig.suptitle('MLPClassifier', fontsize=20)
+# fig.suptitle('MLPClassifier-logistic function', fontsize=20)
 # plt.xlabel('Max_iter', fontsize=16)
 # plt.ylabel('Multi-Class Log Loss', fontsize=16)
 # plt.plot(max_iter_list, mcll_list)
@@ -69,8 +89,8 @@ for i in range(len(pred_y_list)):
 
 # run the best parameter
 start_time = datetime.now()
-mlpb = MLPClassifier(max_iter=22, shuffle=False)
+mlpb = MLPClassifier(solver='adam', max_iter=45, shuffle=False, activation='logistic', random_state=0)
 mlpb.fit(train_x, train_y.values.ravel())
-print(f'The multi_class_log_loss of MLPclassifier is {logloss(mlpb.predict_proba(valid_x), yij)} when max_iter is 22.')
+print(f'The multi_class_log_loss of MLPclassifier is {logloss(mlpb.predict_proba(valid_x), yij)} when max_iter is 45 and activition is logistic.')
 end_time = datetime.now()
 print('Duration: {}'.format(end_time - start_time))
